274619887bc8982014037038dc3bf29489201145
Shin <shinoku1801@gmail.com>
2023-02-07 22:07:40 +0700

    first commit

---
+++ client/script.js
@@ -0,0 +1,138 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const input = document.querySelector('textarea');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+let disable = false;
+
+function loader(element) {
+    element.textContent = '';
+    disable = true;
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+function typeText(element, text) {
+    let index = 0;
+    let scrollInterval = setInterval(() => {
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }, 500);
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            disable = true;
+            element.innerHTML += text.charAt(index);
+            index++;
+        } else {
+            disable = false;
+            chatContainer.scrollTop = chatContainer.scrollHeight;
+            clearInterval(interval);
+            clearInterval(scrollInterval);
+        }
+    }, 20);
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+    return `
+        <div class="wrapper ${isAi ? 'ai' : 'user'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (disable) return;
+
+    const data = new FormData(form);
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    // to clear the textarea input
+    form.reset();
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, '', uniqueId);
+
+    // to focus scroll to the bottom
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div
+    const messageDiv = document.getElementById(uniqueId);
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv);
+
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt'),
+        }),
+    });
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+    if (response.ok) {
+        const data = await response.json();
+        console.log({ data, response });
+        const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
+
+        typeText(messageDiv, parsedData);
+    } else {
+        const error = await response.json();
+        if (error.status === 429) {
+            messageDiv.innerHTML = error.message;
+        } else {
+            messageDiv.innerHTML = 'Something went wrong!';
+        }
+    }
+};
+
+input.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter') {
+        e.preventDefault();
+    }
+});
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e);
+    }
+});
---
+++ server/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openAi = new OpenAIApi(configuration);
+
+const app = express();
+app.use(
+    cors({
+        origin: '*',
+    }),
+);
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX chat bot',
+    });
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openAi.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `${prompt}`,
+            temperature: 0.5, // Higher values means the model will take more risks.
+            max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+            top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+            frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+        });
+        res.status(200).send({
+            bot: response.data.choices[0].text,
+        });
+    } catch (error) {
+        console.log(error.response.status === 429);
+        if (error.response.status === 429) {
+            return res.status(429).send({
+                status: 429,
+                message: 'Too many request, please wait!',
+            });
+        }
+        res.status(500).send({
+            status: 500,
+            message: 'Interval Server Error!',
+        });
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
